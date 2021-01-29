c382a2f3664db51294a66b96d0a3038ae048723e
Julien Orjollet <oj.professionnel@gmail.com>
2021-01-29 11:11:22 +0100

    added the date functionality, and the style.css file

---
+++ welcome-message/script.js
@@ -0,0 +1,40 @@
+const output1 = document.querySelector(".output1");
+const output2 = document.querySelector(".output2");
+
+const button1 = document.querySelector(".button1");
+const button2 = document.querySelector(".button2");
+
+const myName = document.querySelector("input");
+//console.log(button1);
+
+
+
+
+///************************************
+button1.addEventListener("click", showMessage);
+button2.addEventListener("click", showOutput);
+
+
+
+
+///************************************
+function showMessage() {
+  //console.log("Hello Ju");
+  output1.innerHTML = "<h1>Hello welcome, " + myName.value + "</h1 >";
+}
+
+function showOutput() {
+  const date = new Date();
+  let current = date.getHours();
+  let message;
+
+  if (current > 17) message = "its evening";
+  else if (current > 12) message = "its afternoon";
+  else if (current > 0) message = "its morning";
+  else message = "something is wrong";
+
+  output2.innerHTML = "<h1>" + message + "</h1>";
+
+  //console.log(date.getHours());
+  //console.log("works");
+}
