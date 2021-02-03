0eb717c0d48038a66a4a6b6d9888ec7d731d0f0a
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-03 19:14:15 +0100

    added play() func with switch statement

--- play-sound/script.js
+++ play-sound/script.js
@@ -5,13 +5,45 @@ const bark = new Audio('sounds/bark.mp3');
 
 const animals = document.querySelectorAll(".animal");
 
+
+
+
 /// **************************************
 for (let i = 0; i < animals.length; i++) {
   animals[i].addEventListener("click", function () {
     let animal = this.innerHTML;
-    console.log(animal);
+    let lowerAnimal = animal.toLowerCase();
+    makeSound(lowerAnimal)
+    //console.log(animal);
   });
 }
+
+
+
+/// **************************************
+function makeSound(name) {
+  //console.log(name);
+
+  switch (name) {
+    case "lion":
+      let sound1 = new Audio("sounds/lion.mp3");
+      sound1.play();
+      break;
+    case "cougar":
+      let sound2 = new Audio("sounds/cougar.mp3");
+      sound2.play();
+      break;
+    case "dog":
+      let sound3 = new Audio("sounds/bark.mp3");
+      sound3.play();
+      break;
+    /*default
+      break;*/
+  }
+}
+
+
+
 
 /*
 button.addEventListener('click', function () {
