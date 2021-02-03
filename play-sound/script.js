648026167e3102d1ce39df25c721100003b29501
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-03 20:48:23 +0100

    ended refactoring and app

--- play-sound/script.js
+++ play-sound/script.js
@@ -1,25 +1,29 @@
-/// **************************************
-
-const animals = document.querySelectorAll(".animal");
-
-
 
 
 /// **************************************
-for (let i = 0; i < animals.length; i++) {
-  animals[i].addEventListener("click", function () {
-    let animal = this.innerHTML;
-    let lowerAnimal = animal.toLowerCase();
+document.addEventListener("DOMContentLoaded", init);
 
-    playIt(lowerAnimal);
-  });
+/// **************************************
+const myAnimalArr = ["lion", "cougar", "dog"];
+
+
+
+
+/// **************************************
+function init() {
+  //console.log("page ready");
+
+  myAnimalArr.forEach(function (item) {
+    //console.log(item);
+    let div = document.createElement("div");
+    div.setAttribute("class", "animal " + item);
+    div.innerText = item.toUpperCase();
+    div.addEventListener("click", function () {
+      playIt(item);
+    })
+    document.body.appendChild(div);
+  })
 }
-
-
-
-
-
-/// **************************************
 
 
 
@@ -32,31 +36,5 @@ function playIt(name) {
   setTimeout(function () {
     activeElement.classList.remove("active");
   }, 250);
-}
-
-
-function addStyle(name) {
-  let activeElement = document.querySelector("." + name);
-
-  activeElement.classList.add("active");
-  setTimeout(function () {
-    activeElement.classList.remove("active");
-  }, 250);
-}
-
-function makeSound(name) {
-  switch (name) {
-    case "lion":
-
-      break;
-    case "cougar":
-      let sound2 = new Audio("sounds/cougar.mp3");
-      sound2.play();
-      break;
-    case "dog":
-      let sound3 = new Audio("sounds/dog.mp3");
-      sound3.play();
-      break;
-  }
 }
 
