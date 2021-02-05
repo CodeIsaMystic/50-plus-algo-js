5038e46c6912e4132bd815cce1a0a539c0607790
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-05 16:20:15 +0100

    scd method to active only one panel at the time, assigning all actives class in a constant

--- js-accordion/script.js
+++ js-accordion/script.js
@@ -17,14 +17,17 @@ function toggleElements(e) {
   console.log("this: ", this);
   console.log("target event:", e.target);*/
 
+  /* adding only one element atr the time
   accordion.forEach(function (element) {
     element.classList.remove('active');
   });
+  */
 
-
-
-
-
+  const allActives = document.querySelectorAll('.active');
+  //console.log(allActives);
+  allActives.forEach(function (element) {
+    element.classList.remove('active');
+  })
 
   this.classList.toggle('active');
 } 
