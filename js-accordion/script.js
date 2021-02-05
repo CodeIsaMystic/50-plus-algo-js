ffac9a6aedb5e89041f219c6f7f4a21e665b1095
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-05 16:02:44 +0100

    started with implementing the toggleElement function sibling elements

---
+++ js-accordion/script.js
@@ -0,0 +1,19 @@
+
+/// ********************************
+const accordion = document.querySelectorAll(".panel");
+
+
+
+/// ********************************
+accordion.forEach(function (element) {
+  //console.log(element)
+  element.addEventListener("click", toggleElements);
+});
+
+
+/// ********************************
+function toggleElements(e) {
+  console.log("event: ", e);
+  console.log("this: ", this);
+  console.log("target event:", e.target);
+}
