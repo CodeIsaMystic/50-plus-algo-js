e7e4b71e77bb3c9e26264a6f4e972eb901e86477
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-05 15:46:49 +0100

    finished tiny app

--- magic-8ball/script.js
+++ magic-8ball/script.js
@@ -1,10 +1,19 @@
 
 /// ****************************
+const answerArray = ["It will work", "Maybe maybe not", "Highly Likely", "I don't know"];
 const message = document.querySelector(".message");
 const question = document.querySelector("input");
-const button = document.querySelector(".button");
+const button = document.querySelector("button");
 
+/// ****************************
+button.addEventListener("click", function () {
+  console.log(question.value);
 
-
+  let response = Math.floor(Math.random() * answerArray.length);
+  //console.log(answerArray[response]);
+  //console.log(response);
+  message.innerText = question.value + " " + answerArray[response];
+  question.value = "";
+});
 
 
