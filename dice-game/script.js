36a09489dbdb7539d1777cdacd56ea7da0e1b79e
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-08 10:58:39 +0100

    implemented winner logic

--- dice-game/script.js
+++ dice-game/script.js
@@ -2,16 +2,24 @@
 
 /// ***********************************
 const button = document.querySelector("button");
-const player1 = document.querySelector("player1");
-const player2 = document.querySelector("player2");
-const output = document.querySelector("output");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+const output = document.querySelector("#output");
 
 
 /// ***********************************
 button.addEventListener("click", function () {
   //console.log("Works");
   let rolls = [roll(6), roll(6)];
-  console.log(rolls);
+  //console.log(rolls);
+  let temp;
+  if (rolls[0] == rolls[1]) { temp = "It was a draw"; }
+  else if (rolls[0] > rolls[1]) { temp = "Player 1 Wins"; }
+  else { temp = "Player 2 Wins"; }
+
+  output.innerHTML = temp;
+  player1.innerHTML = rolls[0];
+  player2.innerHTML = rolls[1];
 });
 
 
