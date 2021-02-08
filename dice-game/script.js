7f9b6e0be709cdcdd5b64a4e75263557ebc0a344
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-08 12:03:08 +0100

    rebuilt the app creating dynamically and visualy the dice score result

--- dice-game/script.js
+++ dice-game/script.js
@@ -2,24 +2,18 @@
 
 /// ***********************************
 const button = document.querySelector("button");
-const player1 = document.querySelector("#player1");
-const player2 = document.querySelector("#player2");
-const output = document.querySelector("#output");
+const output = document.querySelector(".output");
+const dice = [[5], [1, 9], [1, 5, 9], [1, 3, 7, 9], [1, 3, 5, 7, 9], [1, 3, 4, 6, 7, 9]];
 
 
 /// ***********************************
-button.addEventListener("click", function () {
-  //console.log("Works");
-  let rolls = [roll(6), roll(6)];
-  //console.log(rolls);
-  let temp;
-  if (rolls[0] == rolls[1]) { temp = "It was a draw"; }
-  else if (rolls[0] > rolls[1]) { temp = "Player 1 Wins"; }
-  else { temp = "Player 2 Wins"; }
 
-  output.innerHTML = temp;
-  player1.innerHTML = rolls[0];
-  player2.innerHTML = rolls[1];
+button.addEventListener("click", function () {
+  let rollDice = roll(6);
+  //console.log(rollDice);
+  let holder = builder(rollDice);
+  if (output.children[0]) { output.children[0].remove(); }
+  output.appendChild(holder);
 });
 
 
@@ -27,11 +21,32 @@ button.addEventListener("click", function () {
 
 
 
+
+
 /// **********************************
+function builder(num) {
+  let div = document.createElement("div");
+  let dieArray = dice[num - 1];
+  console.log(dieArray);
+
+  for (let x = 1; x < 10; x++) {
+    let span = document.createElement("div");
+    span.setAttribute("class", "dot");
+    if (dieArray.includes(x)) {
+      span.classList.add("black")
+    }
+    div.appendChild(span);
+  }
+  div.setAttribute("class", "dicer");
+
+  return div;
+}
+
 function roll(num) {
   let rNumber = Math.floor(Math.random() * num) + 1;
-  let n = 9855 + rNumber;
-  let char = "&#" + n + ";";
 
-  return rNumber + " " + char;
+  return rNumber;
 }
+
+
+
