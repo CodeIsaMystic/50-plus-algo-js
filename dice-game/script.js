dc15928228196d00d47465276b4e7c5f27f932da
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-08 13:12:13 +0100

    ending app: rebuild dice game, game read and adjustments

--- dice-game/script.js
+++ dice-game/script.js
@@ -3,17 +3,24 @@
 /// ***********************************
 const button = document.querySelector("button");
 const output = document.querySelector(".output");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
 const dice = [[5], [1, 9], [1, 5, 9], [1, 3, 7, 9], [1, 3, 5, 7, 9], [1, 3, 4, 6, 7, 9]];
 
 
 /// ***********************************
 
 button.addEventListener("click", function () {
-  let rollDice = roll(6);
-  //console.log(rollDice);
-  let holder = builder(rollDice);
-  if (output.children[0]) { output.children[0].remove(); }
-  output.appendChild(holder);
+  let rolls = [roll(6), roll(6)];
+  //console.log(rolls);
+  let temp;
+  if (rolls[0] == rolls[1]) { temp = "Draw" }
+  else if (rolls[0] > rolls[1]) { temp = "Player 1 Wins"; }
+  else { temp = "Player 2 Wins"; }
+  updateOutput(player1, rolls[0]);
+  updateOutput(player2, rolls[1]);
+
+  output.innerHTML = temp;
 });
 
 
@@ -24,10 +31,17 @@ button.addEventListener("click", function () {
 
 
 /// **********************************
+function updateOutput(element, num) {
+  let holder = builder(num);
+  if (element.children[0]) { element.children[0].remove(); }
+  element.appendChild(holder);
+}
+
+
 function builder(num) {
   let div = document.createElement("div");
   let dieArray = dice[num - 1];
-  console.log(dieArray);
+  //console.log(dieArray);
 
   for (let x = 1; x < 10; x++) {
     let span = document.createElement("div");
