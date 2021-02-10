cee2ef758f6d6428d55a78bdefd2bd64d4447d97
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-10 20:32:29 +0100

    implemented randomArray function

--- word-scramble/script.js
+++ word-scramble/script.js
@@ -48,6 +48,13 @@ function createWord() {
 }
 
 
-function randomArray() {
-  console.log("array");
+function randomArray(arr) {
+  //console.log("array");
+  for (let i = arr.length - 1; i > 0; i--) {
+    let temp = arr[i];
+    let j = Math.floor(Math.random() * (i + 1));
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
 }
