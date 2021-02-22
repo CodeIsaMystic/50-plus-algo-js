00001f3f8eddd647020fd3c07c1c77f045de50d0
Julien Orjollet <oj.professionnel@gmail.com>
2021-02-22 19:46:44 +0100

    added popup window code

---
+++ popup-window/script.js
@@ -0,0 +1,17 @@
+const output = document.querySelector(".output");
+const show = document.querySelector(".show");
+const close = document.querySelectorAll(".close");
+const images = document.querySelectorAll(".pop img");
+images.forEach(function (ele) {
+  ele.addEventListener("click", popImage);
+})
+close.forEach(function (ele) {
+  ele.addEventListener("click", function () {
+    show.classList.add("hide");
+  })
+})
+
+function popImage(e) {
+  output.querySelector("img").setAttribute("src", this.src);
+  show.classList.remove("hide");
+}
